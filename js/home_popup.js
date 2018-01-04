0525594e169c6b8e2dc79cd8203dc2307a53ad52
willrocker <icefusion@icefusion.com.br>
2018-01-04 18:01:18 -0200

    Created Modulo to show a popup at the homepage of store.

---
+++ js/home_popup.js
@@ -0,0 +1,8 @@
+$(function() {    
+    $('[data-popup-close]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-close');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+ 
+        e.preventDefault();
+    });
+});
